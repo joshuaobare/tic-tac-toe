1178f7d99835735fa88165f9cced3cdf20ccec5c
joshuaobare <jobare.obare@gmail.com>
2022-06-08 20:18:46 +0300

    added playTurn function to playerFactory

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
 const gameBoard = (() => {
     const board =[]
+    const boardContent =[]
     
 
     const playerFactory = (name,marker) => {
         const getName = () => name;
         const option = () => marker;
-        return {getName,option};
+        const playTurn = (e) => {
+            e.target.innerHTML = marker
+        }
+        
+        
+        return {getName,option,playTurn};
     }
 
-    const player1 = playerFactory('TBD','X')
-    const player2 = playerFactory('TBD','X')
+    const player1 = playerFactory('P1','X')
+    const player2 = playerFactory('P2','O')
 
+
+    
+
+   
     const cell = document.querySelectorAll('.cell')
     cell.forEach((square) => { 
         board.push(square)
         
     square.addEventListener('click', function(e) {
         
-        e.target.innerHTML = 'O'
+        
         console.log(e)
         
 
         function playerSwapper() {
-            if (board[board.length-1] === 'X') {
-                e.target.innerHTML = "O"
+            
+            if (boardContent.length === 0) {
+                player1.playTurn(e)
             } 
     
-            else {
-                e.target.innerHTML = 'X'
+            if (boardContent[boardContent.length-1] === 'X') {
+                player2.playTurn(e)
             }
-    
+
+            if (boardContent[boardContent.length-1] === 'O') {
+                player1.playTurn(e)
+            }
+            
         }
 
         playerSwapper()
-        board.push(e.target.innerHTML)
+        boardContent.push(e.target.innerHTML)
+        console.log(boardContent)
         console.log(square)
     },{once:true})
 
